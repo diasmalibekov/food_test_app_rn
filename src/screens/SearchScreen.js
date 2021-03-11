2fc389731bc90078d315ae0045e41d88ce2053fd
diasmalibekov <67695163+diasmalibekov@users.noreply.github.com>
2021-03-11 18:34:35 +0500

    https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15707166#overview

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
+import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
+    const [searchApi, results, errorMessage] = useResults()
 
     return <View>
     <SearchBar
         term={ term }
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted')}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
     />
-    <Text>Search Screen</Text>
-    <Text> { term } </Text>
+    {errorMessage ? <Text>{ errorMessage }</Text> : null}
+    <Text>We have found { results.length } results </Text>
+    <ResultsList title='Cost Effective'/>
+    <ResultsList title='Bit Pricier'/>
+    <ResultsList title='Big Spender'/>
     </View>
 }
 
